aab66a9de9c3b5a96c77e8e2b382bb4d2f78bc51
Gloria Visus <gloriavisus@gmail.com>
2019-07-11 16:14:56 +0200

    final

--- services/NailPolishService.js
+++ services/NailPolishService.js
@@ -1,7 +1,7 @@
 'use strict';
 function NailPolishService(){
-    this.baseUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type='
-    this.singleUrl = 'http://makeup-api.herokuapp.com/api/v1/products/'
+    this.baseUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=';
+    this.singleUrl = 'http://makeup-api.herokuapp.com/api/v1/products/';
 }
 NailPolishService.prototype.getAllNailPolish = async function(ending){
     var response = await fetch(`${this.baseUrl}${ending}`);
